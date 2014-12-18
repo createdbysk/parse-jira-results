6e5dcb84106b16b74cd25b56af7d7a8d165e9710
Satish Kumar <skumar@socialware.com>
2014-12-18 15:29:27 -0600

    Move the test code into the experiment so that you can use the experiment with the original results from JIRA. This is preparation for the changes to make the results be a mock in the test.

--- parse_jira_results/experiment/jiraResultsParser.1.experiment.js
+++ parse_jira_results/experiment/jiraResultsParser.1.experiment.js
@@ -1,11 +1,26 @@
-var requirejs;
-requirejs = require('requirejs'); 
+// ALWAYS RUN THIS WITH CURRENT WORKING DIRECTORY AS THE ROOT OF THIS PROJECT.
+var fs,
+    linq,
+    results;
+fs = require('fs');
+linq = require('linq');
+results = JSON.parse(fs.readFileSync('test_input/SW-14155.txt', 'utf-8'));
 
-requirejs.config({
-    baseUrl: __dirname,
-    nodeRequire: require
-});
-
-requirejs(['fs'], function (fs) {
-	
-});
+issues = linq.from(results.issues)
+    .select(function (issue) {
+        var statuses;
+        statuses = linq.from(issue.changelog.histories)
+            .select(function (history) {
+                var status;
+                status = linq.from(history.items)
+                    .where(function (item) {
+                        return item.field === 'status';
+                    })
+                    .select(function (item) {
+                        return {from: item.fromString, to: item.toString};
+                    });
+                return {date: history.created, status: status.toArray()};
+            });
+        return {key: issue.key, statuses: statuses.toArray()};
+    });
+console.log(JSON.stringify(issues.toArray(), undefined, 4));
--- parse_jira_results/test/parseJiraResults.test.js
+++ parse_jira_results/test/parseJiraResults.test.js
@@ -7,11 +7,34 @@ describe('parse jira results', function () {
     var results;
 
     beforeEach(function (done) {
-        results = JSON.parse(fs.readFileSync('test_input/SW-14155.txt', 'utf-8'));
+        //results = JSON.parse(fs.readFileSync('test_input/SW-14155.txt', 'utf-8'));
+        resultsJSON = {  
+           "issues":[  
+              {  
+                 "key":"SW-14155",
+
         done();
     });
     it('reads extracts the first and last states of an issue status', function (done) {
-        var issues;
+        var expectedIssues,
+            issues;
+        expectedIssues = [
+            {
+                key: "SW-14155",
+                statuses: [
+                    {
+                        date: "2014-12-01T15:58:25.000+0000",
+                        from: "Triage",
+                        to: "Open"
+                    },
+                    {
+                        date: "2014-12-18T12:12:21.000+0000",
+                        from: "Ready for Testing",
+                        to: "Closed"
+                    }
+                ]
+            }        
+        ];
         issues = linq.from(results.issues)
             .select(function (issue) {
                 var statuses;
