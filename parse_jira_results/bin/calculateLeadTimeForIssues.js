7a6c3059ed0bcf9537ea5ec4100977f65f76875b
Satish Kumar <kumarnotifications@gmail.com>
2014-12-19 20:31:35 -0600

    Implement the ability to calculate leadtimes for issues.

--- parse_jira_results/bin/calculateLeadTimeForIssues.js
+++ parse_jira_results/bin/calculateLeadTimeForIssues.js
@@ -2,16 +2,50 @@
 var requirejs;
 requirejs = require('../configuredRequirejs.js');
 
-requirejs(['fs', 'linq', 'lib/issueStatusExtractor', 'lib/statusFilter'], 
-    function (fs, linq, issueStatusExtractor, statusFilter) {
+requirejs(['fs', 
+          'linq', 
+          'lib/issueStatusExtractor', 
+          'lib/statusFilter',
+          'lib/leadTimeCalculator'], 
+    function (fs, linq, issueStatusExtractor, statusFilter, leadTimeCalculator) {
         fs.readFile('test_input/SW-14155.txt', 'utf-8', function (err, resultsJSON) {
             var results;
             results = JSON.parse(resultsJSON);
-            issueStatusExtractor(results, function (err, issuesStatus) {
-                console.log(JSON.stringify(issuesStatus.toArray(), undefined, 4));
-                linq.from(issuesStatus)
+            issueStatusExtractor(results, function (err, issuesWithStatuses) {
+                var leadTimes;
+                leadTimes = linq.from(issuesWithStatuses)
                     .select(function (issue) {
-                    })
+                        var startDate,
+                            endDate;
+                        statusFilter(issue.statuses, 
+                            function (status) {
+                                return status.from === "Open" &&
+                                    status.to !== "Triage" &&
+                                    status.to !== "Closed";
+                            },
+                            function (error, possibleCommitmentPoints) {
+                                startDate = linq.from(possibleCommitmentPoints)
+                                                .select(function (status) {
+                                                    return status.date;
+                                                })
+                                                .firstOrDefault();
+                            }
+                        );
+                        statusFilter(issue.statuses, 
+                            function (status) {
+                                return status.to === "Closed";
+                            },
+                            function (error, possibleExitPoints) {
+                                endDate = linq.from(possibleExitPoints)
+                                                .select(function (status) {
+                                                    return status.date;
+                                                })
+                                                .lastOrDefault();
+                            }
+                        );
+                        return {key: issue.key, leadTime: leadTimeCalculator(startDate, endDate)};
+                    });
+                console.log(JSON.stringify(leadTimes.toArray(), undefined, 4));
             });
         })
     }
