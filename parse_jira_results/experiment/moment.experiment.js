d399ac7935c04849bdb0a3a3f12c7f6848e542e9
Satish Kumar <kumarnotifications@gmail.com>
2015-01-08 21:07:09 -0600

    Add an experiment for diff.

--- parse_jira_results/experiment/moment.experiment.js
+++ parse_jira_results/experiment/moment.experiment.js
@@ -3,7 +3,8 @@ var moment,
     dateToConvert,
     anotherDateToConvert,
     date1,
-    date2;
+    date2,
+    diff;
 // This call adds 'static' methods to the JS Date class.
 moment = require('moment');
 // Display today.
@@ -17,3 +18,9 @@ anotherDateToConvert = '2014-12-18T12:12:21.000+0000';
 date2 = moment(anotherDateToConvert);
 console.log(anotherDateToConvert, date2.format("YYYY-MM-DD HH:MM:SS"), "DAY", date2.date());
 console.log("LEAD TIME", date2.startOf('day').diff(date1.startOf('day'), 'days'));
+
+date1 = "2014-12-00T15:58:25.000+0000";
+date2 = "2014-12-17T12:12:21.000+0000";
+
+diff = moment(date2).diff(moment(date1), 'days', true);
+console.log('DIFF', diff);
