4266f4b47b890d2df391bda1f3076760e5afe94d
satvidh <satvidh@gmail.com>
2015-05-23 07:25:45 -0500

    Implement query jira and transform results.

--- parse_jira_results/bin/storeJiraResultsInGoogleSheet.js
+++ parse_jira_results/bin/storeJiraResultsInGoogleSheet.js
@@ -15,25 +15,55 @@ requirejs(['commander',
         readFileAndIterate,
         linq,
         path,
-        issueStatusExtractor,
-        issuePriorityExtractor,
         transformLoader,
         transformer,
-        jiraRest)
+        JiraRest)
     {
         'use strict';
         var extractors,
             processIssue,
             processResults,
-            displayResults;
+            displayResults,
+            jiraRest;
 
         program
             .version('0.0.1')
             .usage('configuration_file_name')
             .parse(process.argv);
 
-        requirejs(program.args[0], function (configuration) {
-            console.log("CONFIGURATION", configuration);
+        requirejs([program.args[0]], function (configuration) {
+            jiraRest = JiraRest(configuration.jiraConfiguration);
+            jiraRest.search(program.args[1], function (err, result) {
+                console.log("RESULT", result);
+                transformLoader.loadModules(configuration.moduleConfiguration, function (err, transforms) {
+                    var fields;
+                    processIssue =
+                        function (issue) {
+                            var resultsWithExtractedFields;
+                            transformer(issue, transforms, function (err, extractedFields) {
+                                resultsWithExtractedFields = extractedFields;
+                            });
+                            return resultsWithExtractedFields;
+                        };
+                    processResults = function (results) {
+                        var issuesWithExtractFields;
+                        issuesWithExtractFields =
+                            linq.from(results.issues)
+                                .select(processIssue);
+                        return issuesWithExtractFields;
+                    };
+                    displayResults = function (err, allIssuesWithExtractFields) {
+                        linq.from(allIssuesWithExtractFields)
+                            .forEach(function (issueWithExtractFields) {
+                                console.log(JSON.stringify(issueWithExtractFields));
+                            });
+                    };
+
+                    fields = processResults(result);
+                    displayResults(null, fields);
+                });
+            });
+
         });
     //     transformLoader.loadModules(program.extractor, function (err, transforms) {
     //         processIssue =
