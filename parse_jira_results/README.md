66c6bfd9af6e14a3a5928b9473efc975fafbe625
satvidh <satvidh@gmail.com>
2015-05-30 16:23:35 -0500

    Experiment with working version of write o Google spreadsheet.

--- parse_jira_results/README.md
+++ parse_jira_results/README.md
@@ -2,7 +2,7 @@ Implements the ability to parse the results from the JIRA rest API queries.
 
 # Usage
 ## Extract Fields
-Use bin/extractFields.js to extract fields from the raw JSON. Pass the extractors as follows 
+Use bin/extractFields.js to extract fields from the raw JSON. Pass the extractors as follows
 
 ```
 node bin/extractFields.js -e lib/issueStatusExtractor,status -e lib/issuePriorityExtractor,priority <raw json filename>
@@ -19,7 +19,7 @@ In the example below, bin/extractFields.sh is a convenience script that invokes 
 bin/extractFields.sh <raw json filename> | node bin/calculateMetrics.js -o <comma separated list of statuses> <name of field to report>
 ```
 
-The output is a csv. 
+The output is a csv.
 
 For example,
 
@@ -30,5 +30,18 @@ bin/extractFields.sh <raw json filename> | node bin/calculateMetrics.js -o "Tria
 # Library use
 * Create an experiment under the experiment directory to learn how a library works.
 
+# References
+## Links
+[Accessing Google Spreadsheets from NodeJs]( http://www.nczonline.net/blog/2014/03/04/accessing-google-spreadsheets-from-node-js/)
+[Google Oauth2 playground](https://developers.google.com/oauthplayground/)
+[Google Javascript Client API](https://developers.google.com/api-client-library/javascript/start/start-js)
+[Google NodeJs Client API](https://github.com/google/google-api-nodejs-client)
 
+## How to generate key file
+// Follow directions on this page at -https://github.com/google/google-api-nodejs-client
+// under Using JWT (Service Tokens)
+// Use the following command to generate the .pem file from the
+// .p12 file downloaded from the Google Developers Console
+// openssl pkcs12 -in key.p12 -nocerts -passin pass:notasecret -nodes -out key.pem
+// put the .pem file in the same directory as this script.
 DO NOT CHECK IN JIRA RESULTS.
---
+++ parse_jira_results/experiment/google/googleSpreadsheet.experiment.js
@@ -0,0 +1,57 @@
+var GoogleSpreadsheet,
+    mySheet,
+    googleapis,
+    jwtClient,
+    auth;
+
+GoogleSpreadsheet = require('google-spreadsheet');
+googleapis = require('googleapis');
+// Follow directions on this page at -https://github.com/google/google-api-nodejs-client
+// under Using JWT (Service Tokens)
+// Use the following command to generate the .pem file from the
+// .p12 file downloaded from the Google Developers Console
+// openssl pkcs12 -in key.p12 -nocerts -passin pass:notasecret -nodes -out key.pem
+// put the .pem file in the same directory as this script.
+jwtClient = new  googleapis.auth.JWT(
+    '297654144845-3mkk4rmp9sbpr0e7gvac3gka7u3484ct@developer.gserviceaccount.com',
+    '2cad7fbdbae95800c9c1a4e59f47d594e220765c-privatekey.pem',
+    null,
+    // scope from https://developers.google.com/google-apps/spreadsheets/#authorizing_requests_with_oauth_20
+    'https://spreadsheets.google.com/feeds'
+);
+
+jwtClient.authorize(function (err, tokens) {
+    var auth;
+    if (err) {
+        console.log("ERROR", err);
+    }
+    else {
+        console.log("TOKEN", tokens);
+        auth = {
+            type: tokens.token_type,
+            value: tokens.access_token
+        };
+        // Access the spreadsheet at
+        // https://docs.google.com/spreadsheets/d/1krSj0KrswrtC3hCgohXfyYaItkfvVOf_mMQICSAA2G4/edit?usp=sharing
+        // Notice the key below matches the key in the url above.
+        mySheet = new GoogleSpreadsheet('1krSj0KrswrtC3hCgohXfyYaItkfvVOf_mMQICSAA2G4', auth);
+        // Access the rows of the first worksheet
+        mySheet.getRows(1, function (err, rowData) {
+            console.log("Error", err);
+            console.log("RowData", JSON.stringify(rowData));
+        });
+        // Add a row to second worksheet.
+        mySheet.addRow(2, {This:"25", is:"345", a:"3059", test:'ueteu'}, function (err) {
+            console.log("ADD ERR", err);
+        });
+        // Get info about the worksheets.
+        mySheet.getInfo( function(err, sheet_info) {
+             if (err) {
+                 console.log("ERROR", err);
+             }
+             else {
+                 console.log( 'pulled in '+JSON.stringify(sheet_info));
+             }
+        });
+    }
+});
