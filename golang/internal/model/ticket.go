7f7cf0eb665cf5531285eda67eaee6ff23f74299
Satish Kumar <skumar@datto.com>
2021-04-03 20:15:21 -0500

    Make order consistent with how it will be in the output

--- golang/internal/model/ticket.go
+++ golang/internal/model/ticket.go
@@ -4,8 +4,8 @@ import "time"
 
 type Ticket struct {
 	Name      string    `json: "name"`
-	StartDate time.Time `json: "startDate"`
-	EndDate   time.Time `json: "endDate"`
 	Priority  string    `json: "priority"`
 	Type      string    `json: "type"`
+	StartDate time.Time `json: "startDate"`
+	EndDate   time.Time `json: "endDate"`
 }
--- golang/internal/model/ticket_test.go
+++ golang/internal/model/ticket_test.go
@@ -11,19 +11,19 @@ func TestUnmarshalJSON(t *testing.T) {
 	// GIVEN
 	serialized, _ := json.Marshal(map[string]string{
 		"name":      "TICKET-1",
-		"startDate": "2021-03-01T00:00:00Z",
-		"endDate":   "2021-03-02T00:00:00Z",
 		"priority":  "3-Low",
 		"type":      "Task",
+		"startDate": "2021-03-01T00:00:00Z",
+		"endDate":   "2021-03-02T00:00:00Z",
 	})
 	startDate, _ := time.Parse("2006-01-02", "2021-03-01")
 	endDate, _ := time.Parse("2006-01-02", "2021-03-02")
 	expected := Ticket{
 		Name:      "TICKET-1",
-		StartDate: startDate,
-		EndDate:   endDate,
 		Priority:  "3-Low",
 		Type:      "Task",
+		StartDate: startDate,
+		EndDate:   endDate,
 	}
 
 	// WHEN
