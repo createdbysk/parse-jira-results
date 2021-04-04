fe7716a27feefb9582c666c49ef0561dc86c2bf1
Satish Kumar <skumar@datto.com>
2021-04-04 08:11:57 -0500

    Add more fields

--- golang/internal/model/ticket.go
+++ golang/internal/model/ticket.go
@@ -3,9 +3,12 @@ package model
 import "time"
 
 type Ticket struct {
-	Name      string    `json: "name"`
-	Priority  string    `json: "priority"`
-	Type      string    `json: "type"`
-	StartDate time.Time `json: "startDate"`
-	EndDate   time.Time `json: "endDate"`
+	Name       string    `json: "name"`
+	Priority   string    `json: "priority"`
+	Type       string    `json: "type"`
+	Status     string    `json: "status"`
+	Resolution string    `json: "resolution"`
+	Sprint     string    `json: "sprint"`
+	StartDate  time.Time `json: "startDate"`
+	EndDate    time.Time `json: "endDate"`
 }
--- golang/internal/model/ticket_test.go
+++ golang/internal/model/ticket_test.go
@@ -10,20 +10,27 @@ import (
 func TestUnmarshalJSON(t *testing.T) {
 	// GIVEN
 	serialized, _ := json.Marshal(map[string]string{
-		"name":      "TICKET-1",
-		"priority":  "3-Low",
-		"type":      "Task",
-		"startDate": "2021-03-01T00:00:00Z",
-		"endDate":   "2021-03-02T00:00:00Z",
+		"name":        "TICKET-1",
+		"priority":    "3-Low",
+		"type":        "Task",
+		"status":      "Open",
+		"resolution":  "Unresolved",
+		"sprint":      "Sprint",
+		"createdDate": "2021-02-28T00:00:00Z",
+		"startDate":   "2021-03-01T00:00:00Z",
+		"endDate":     "2021-03-02T00:00:00Z",
 	})
 	startDate, _ := time.Parse("2006-01-02", "2021-03-01")
 	endDate, _ := time.Parse("2006-01-02", "2021-03-02")
 	expected := Ticket{
-		Name:      "TICKET-1",
-		Priority:  "3-Low",
-		Type:      "Task",
-		StartDate: startDate,
-		EndDate:   endDate,
+		Name:       "TICKET-1",
+		Priority:   "3-Low",
+		Type:       "Task",
+		Status:     "Open",
+		Resolution: "Unresolved",
+		Sprint:     "Sprint",
+		StartDate:  startDate,
+		EndDate:    endDate,
 	}
 
 	// WHEN
